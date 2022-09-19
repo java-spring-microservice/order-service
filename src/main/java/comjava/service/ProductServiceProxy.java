066431c62ad3a2f954d15449d6b691149c3bbf59
Tien Huynh Anh <hatien@tma.com.vn>
2022-09-19 17:27:30 +0700

    update

--- src/main/java/comjava/service/ProductServiceProxy.java
+++ src/main/java/comjava/service/ProductServiceProxy.java
@@ -1,13 +1,12 @@
 package comjava.service;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import comjava.dto.ProductDTO;
 
 @FeignClient(name = "product-service")
-@RibbonClient(name = "product-service")
+// @RibbonClient(name = "product-service")
 public interface ProductServiceProxy {
 
     @GetMapping("/products/{productId}")
